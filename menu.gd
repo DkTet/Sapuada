1a38809c121da6984b021686acfcfe03eca9d7ea
Gabriel Vitorio Goncalves <104917512+GabrielVitorioGoncalves@users.noreply.github.com>
2025-09-12 20:21:00 -0300

    final
    Jogo feito

--- menu.gd
+++ menu.gd
@@ -3,7 +3,7 @@ func _ready():
 	$VBoxContainer/StartButton.grab_focus()
 	
 func _on_start_button_pressed() -> void:
-	get_tree().change_scene_to_file("res://scenes/fase_1.tscn")
+	get_tree().change_scene_to_file("res://scenes/fase 1 certo.tscn")
 
 func _on_quit_button_pressed() -> void:
 	get_tree().quit()
